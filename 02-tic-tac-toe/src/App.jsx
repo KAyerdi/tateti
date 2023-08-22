5ee8c73d73a76f239ada7e96cf3914c241942db5
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-08-22 11:30:03 -0300

    actualizando

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -7,9 +7,16 @@ const TURNS = {
 
 
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({children, isSelected, updateBoard, index}) => {
+
+  const className= `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-  <div className="square">
+  <div onClick={handleClick} className={className}>
   {children}
   </div>
   )
@@ -17,7 +24,13 @@ const Square = ({children, updateBoard, index}) => {
 
 function App() {
   const [board, setBoard]  = useState(Array(9).fill(null))
+
   const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    const newTurn = turn == TURN.X ? TURN.O : TURN.X
+    setTurn(newTurn)
+  }
   return (
     <main className="board">
       <h1>TIC TAC TOE</h1>
@@ -28,12 +41,19 @@ function App() {
           <Square
             key={index}
             index={index}
+            updateBoard={updateBoard}
             >
               {board [index]}
             </Square>
           )
         })
       }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn == TURNS.X}> {TURNS.X}
+        </Square>
+        <Square isSelected={turn == TURNS.O}>{TURNS.O}
+        </Square>
       </section>
     </main>
   )
